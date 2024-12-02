2cd19222b32b5a9d0fb0dc6d77be3a143519e83c
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2024-12-02 22:33:37 +0000

    Solve day 1, part 2

--- day-1/app.py
+++ day-1/app.py
@@ -1,11 +1,12 @@
 """ Day 1: Historian Hysteria """
 
-from typing import Collection, Generator, Iterable, Sequence, Tuple
+from typing import Collection, Counter, Generator, Iterable, Sequence, Tuple
 
 
 def read_input() -> Generator[Tuple[int, int], None, None]:
+    """Lazily read and parse the input file into (x, y) pairs"""
     file: Iterable[str]
-    with open("input") as file:
+    with open("input", encoding="utf-8") as file:
         line: str
         for line in file:
             lhs: int
@@ -15,24 +16,35 @@ def read_input() -> Generator[Tuple[int, int], None, None]:
             yield lhs, rhs
 
 
-# Load the entire input into memory
-input: Collection[Tuple[int, int]] = tuple(read_input())
+def main() -> None:
+    """Solution for AoC 2024, Day 1, Parts 1 & 2"""
+    # --- Part One ---
 
-# Split into columns and sort
-locations_lhs: Sequence[int]
-locations_rhs: Sequence[int]
-locations_lhs, locations_rhs = map(sorted, zip(*input))
+    # Load the entire dataset into memory
+    inputs: Collection[Tuple[int, int]] = tuple(read_input())
 
-# Total distance between all locations
-total_distance: int = sum(
-    abs(lhs - rhs) for lhs, rhs in zip(locations_lhs, locations_rhs)
-)
+    # Split into columns and sort
+    locations_lhs: Sequence[int]
+    locations_rhs: Sequence[int]
+    locations_lhs, locations_rhs = map(sorted, zip(*inputs))
 
-print(total_distance)
+    # Total distance between all locations
+    total_distance: int = sum(
+        abs(lhs - rhs) for lhs, rhs in zip(locations_lhs, locations_rhs)
+    )
+
+    print("Total Distance:", total_distance)
+
+    # --- Part Two ---
+
+    location_counts: Counter[int] = Counter(locations_rhs)
+
+    similarity_score: int = sum(
+        value * location_counts[value] for value in locations_lhs
+    )
+
+    print("Similarity Score:", similarity_score)
 
 
-# def main() -> None:
-#     ...
-
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
