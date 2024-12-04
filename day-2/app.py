a2ed5808b2c8ad555865e0cc5d90a3f5a05e49a1
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2024-12-04 08:43:03 +0000

    Remove commented-out code

--- day-2/app.py
+++ day-2/app.py
@@ -70,188 +70,6 @@ def is_report_safe(report: Report, /) -> bool:
     return True
 
 
-# wrong: 323, 328, 329, 315, 334, 344
-# ans: 349
-# def is_safe_with_dampening(
-#     report: Report, / #, *, expected_sign: Optional[int] = None
-# ) -> bool:
-#     """Determine whether a report is considered safe (with dampening)"""
-
-#     # print(report)
-
-#     # If the report contains too few levels to be considered unsafe, it is safe.
-#     if len(report) < 2:
-#         return True
-
-#     # if expected_sign is None:
-#     #     expected_sign = get_sign(report[1] - report[0])
-
-#     # previous_lhs: Optional[int] = None
-
-#     # index: int
-#     # lhs: int
-#     # for index, lhs in enumerate(report[:-1]):
-#     #     is_first_pair: bool = index == 0
-#     #     rhs: int = report[index + 1]
-
-#     #     print(
-#     #         index,
-#     #         (lhs, rhs),
-#     #         dict(expected_sign=expected_sign, previous_level=previous_lhs),
-#     #     )
-
-#     #     if not is_pair_safe(lhs, rhs, expected_sign=expected_sign):
-#     #         return is_safe_with_dampening(
-#     #             (
-#     #                 *((previous_lhs,) if previous_lhs is not None else ()),
-#     #                 rhs,
-#     #                 *report[index + 1 :],
-#     #             ),
-#     #             expected_sign=(
-#     #                 None if is_first_pair else expected_sign
-#     #             )
-#     #         )
-
-#     #     previous_lhs = lhs
-
-#     # return False # TEMP!
-
-#     # has_bad_level: bool = False  # we haven't (yet) seen any bad levels
-#     # lhs: int = report[0]  # the first level in the report is the first pair's lhs
-#     # sign: Optional[int] = None
-#     # is_previous_pair_unsafe: bool = False
-#     # total_bad_levels: int = 0
-#     has_bad_level: bool = False
-#     sign: Optional[int] = None
-#     previous_lhs: Optional[int] = None
-
-#     # index: int
-#     # rhs: int
-#     # for index, rhs in enumerate(report[1:]):
-#     index: int
-#     lhs: int
-#     for index, lhs in enumerate(report[:-1]):
-#         rhs: int = report[index + 1]
-
-#         # print(index, (lhs, rhs))
-
-#         if sign is None:
-#             sign = get_sign(rhs - lhs)
-
-#         if not is_pair_safe(lhs, rhs, expected_sign=sign):
-#             if has_bad_level:
-#                 return False
-
-#             has_bad_level = True
-
-#             if previous_lhs is not None:
-#                 # try and fix (might need to recalc sign)
-#                 if index == 1:
-#                     sign = get_sign(rhs - previous_lhs)
-
-#                 if not is_pair_safe(previous_lhs, rhs, expected_sign=sign):
-#                     if index == 1:
-#                         sign = None
-#                     # the rhs is bad, promote the lhs
-#                     # (don't change the lhs)
-#                     continue
-#                 ...
-
-#         previous_lhs = lhs
-#         lhs = rhs
-
-
-#         # # If the previous pair was unsafe (and couldn't be fixed), the current lhs is bad,
-#         # # so replace it with the previous lhs
-#         # if is_previous_pair_unsafe:
-#         #     lhs = previous_lhs
-#         #     # if sign is None:
-#         #     #     sign = get_sign(rhs - previous_lhs)
-
-#         #     # if not is_pair_safe(previous_lhs, rhs, expected_sign=sign):
-#         #     #     return False
-
-#         #     # # Our lhs is the bad level, so don't bother checking it.
-#         #     # is_previous_pair_unsafe = False
-#         # # else:
-#         # if sign is None:
-#         #     sign = get_sign(rhs - lhs)
-
-#         # if not is_pair_safe(lhs, rhs, expected_sign=sign):
-#         #     is_previous_pair_unsafe = True
-#         #     # total_bad_levels += 1
-
-#         #     if total_bad_levels > MAX_BAD_LEVELS:
-#         #         return False
-
-#         #     if index == 0:
-#         #         sign = None
-
-#         #     if previous_lhs is not None:
-#         #         if sign is None:
-#         #             sign = get_sign(rhs - previous_lhs)
-
-#         #         if is_pair_safe(previous_lhs, rhs, expected_sign=sign):
-#         #             is_previous_pair_unsafe = False
-
-
-#         # # print(index, (lhs, rhs))
-#         # # Only if this is the first pair, or the first pair was unsafe, set
-#         # # the expected sign of the report
-#         # # if index == 0: # or index == 1 and has_bad_level:
-#         # if sign is None:
-#         #     sign = get_sign(rhs - lhs)
-
-#         # # if sign is None:
-#         # #     sign = get_sign(rhs - lhs)
-
-#         # # If the pair is not safe, then suspect the lhs is bad.
-#         # # If it's the first bad level, let's try and remove the lhs to see if
-#         # # that makes the report safe (otherwise the report is unsafe)
-#         # if not is_pair_safe(lhs, rhs, expected_sign=sign):
-#         #     # We've seen two unsafe levels, therefore the report is unsafe.
-#         #     if has_bad_level:
-#         #         # print()
-#         #         return False
-
-#         #     # Mark that we've seen a bad level
-#         #     has_bad_level = True
-
-#         #     # if index == 0:
-#         #     #     sign = None
-
-#         #     # # Test the values either side of the bad level
-#         #     # es2 = sign
-#         #     # if index == 1:
-#         #     #     es2 = get_sign(rhs - previous_lhs)
-
-#         #     # if index == 1:
-#         #     #     sign =
-
-#         #     if index == 0:
-#         #         sign = None
-#         #     elif index == 1:
-#         #         sign = get_sign(rhs - previous_lhs)
-
-#         #     if previous_lhs is not None and not is_pair_safe(
-#         #         previous_lhs, rhs, expected_sign=sign
-#         #     ):
-#         #         # The levels either side of the bad level aren't safe, so the report
-#         #         # isn't safe even with this problematic level removed.
-#         #         return False
-#         # # else:
-#         #     # Only set the next pair's lhs to our rhs if the pair was safe.
-#         #     # This promotes our lhs to be the next pair's lhs if the pair is
-#         #     # unsafe and effectively skips the current bad (rhs) level.
-#         #     # lhs = rhs
-
-
-#     # print()
-
-#     # The report has at most one bad level, and is therefore safe.
-#     return True
-
-
 def is_report_safe_2(
     report: Report, /, *, dampen: bool = True, expected_sign: Optional[int] = None
 ) -> bool:
@@ -262,6 +80,7 @@ def is_report_safe_2(
     for index, lhs in enumerate(report[:-1]):
         rhs: int = report[index + 1]
 
+        # If the sign is unknown, calculate it.
         if expected_sign is None:
             expected_sign = get_sign(rhs - lhs)
 
@@ -269,28 +88,40 @@ def is_report_safe_2(
         if is_pair_safe(lhs, rhs, expected_sign=expected_sign):
             continue
 
+        # Otherwise, the pair is unsafe, let's take a closer look.
+
+        # If dampening is disabled, one bad pair means the entire report is unsafe.
         if not dampen:
             return False
 
-        safe_with_plhs_removed: bool = True
-        if index == 1:
-            # try removing first value
-            safe_with_plhs_removed = is_report_safe_2(report[1:], dampen=False, expected_sign=None)
-
-        # try and remove lhs
-        report_with_lhs_removed: Report = report[1:] if index == 0 else (report[index-1], *report[index+1:])
+        # See if the report is safe with the lhs removed
+        report_with_lhs_removed: Report = (
+            report[1:] if index == 0 else (report[index - 1], *report[index + 1 :])
+        )
         sign_with_lhs_removed: Optional[int] = None if index < 2 else expected_sign
-        safe_with_lhs_removed: bool = is_report_safe_2(report_with_lhs_removed, dampen=False, expected_sign=sign_with_lhs_removed)
+        safe_with_lhs_removed: bool = is_report_safe_2(
+            report_with_lhs_removed, dampen=False, expected_sign=sign_with_lhs_removed
+        )
 
-        # try and remove rhs
-        report_with_rhs_removed: Report = (lhs, *report[index+2:])
+        # See if the report is safe with the rhs removed
+        report_with_rhs_removed: Report = (lhs, *report[index + 2 :])
         sign_with_rhs_removed: Optional[int] = None if index == 0 else expected_sign
-        safe_with_rhs_removed: bool = is_report_safe_2(report_with_rhs_removed, dampen=False, expected_sign=sign_with_rhs_removed)
+        safe_with_rhs_removed: bool = is_report_safe_2(
+            report_with_rhs_removed, dampen=False, expected_sign=sign_with_rhs_removed
+        )
 
-        if index == 1:
-            return safe_with_plhs_removed or safe_with_lhs_removed or safe_with_rhs_removed
-        else:
-            return safe_with_lhs_removed or safe_with_rhs_removed
+        # If the report is safe with either the lhs, or rhs removed, then the
+        # report can be considered safe (through the power of dampening)
+        if safe_with_lhs_removed or safe_with_rhs_removed:
+            return True
+
+        # Otherwise, the report is most likely unsafe, however if the second pair
+        # is "bad", then this could be because the first level in the report is bad.
+        # If the first level in the report is bad, then the sign will likely be wrong,
+        # so we should test the safety of the report with the first level removed.
+        return index == 1 and is_report_safe_2(
+            report[1:], dampen=False, expected_sign=None
+        )
 
     # The report has not been deemed unsafe, and is therefore safe.
     return True
