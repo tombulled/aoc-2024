2b4c7d6fb91faf0fa8b5fee8096774a61b21ec90
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2024-12-05 22:46:31 +0000

    day 3 part 2: setup stream of instructions ready for execution

--- day-3/app.py
+++ day-3/app.py
@@ -1,9 +1,9 @@
 """Day 3: Mull It Over"""
 
-from enum import Enum
-import operator
+from dataclasses import dataclass
 import re
-from typing import Final, Iterable, Optional, Sequence
+from enum import Enum
+from typing import Final, Generator, Iterable, Optional, Sequence
 
 
 class Operator(str, Enum):
@@ -15,31 +15,38 @@ class Operator(str, Enum):
     DONT: str = "don't"
 
 
-def _build_instruction_pattern(
-    # operator: Operator, operands: Optional[str] = None, /
-    operator: Operator, *operands: str
-) -> str:
-    # return f"(?P<instruction>(?P<{operator}>[a-z]+)\({operands}\))"
-    # return f"(?P<operator>{operator})\({operands or ''}\)"
-    return f"{operator}\({','.join(operands)}\)"
+@dataclass
+class Instruction:
+    operator: Operator
+    operands: Sequence[str]
 
 
-PATTERN_OPERATOR: Final[str] = f"(?P<operator>{'|'.join(Operator)})"
+def _build_instruction_pattern(operator: Operator, *operands: str) -> str:
+    return rf"{operator}\({','.join(operands)}\)"
 
-OPERATOR: Final[str] = r"[a-z]+"
+
+PATTERN_OPERATOR: Final[str] = rf"(?P<operator>{'|'.join(Operator)})"
+
+OPERATOR: Final[str] = r"[a-z']+"
 OPERAND: Final[str] = r"\d{1,3}"
-OPERANDS: Final[str] = f"(?:{OPERAND},)*{OPERAND}?"
+# OPERANDS: Final[str] = rf"(?:{OPERAND},)*{OPERAND}?"
 
-NO_OPERANDS: Final[str] = ""
-OPERANDS_DO: Final[str] = NO_OPERANDS
-OPERANDS_DONT: Final[str] = NO_OPERANDS
-OPERANDS_MUL: Final[str] = r"(?P<lhs>\d{1,3}),(?P<rhs>\d{1,3})"
+# NO_OPERANDS: Final[str] = ""
+# OPERANDS_DO: Final[str] = NO_OPERANDS
+# OPERANDS_DONT: Final[str] = NO_OPERANDS
+# OPERANDS_MUL: Final[str] = r"(?P<lhs>\d{1,3}),(?P<rhs>\d{1,3})"
 
-PATTERN_INSTRUCTION: Final[str] = r"(?P<instruction>)\((?P<operands>.*?)\)"
-PATTERN_MUL: Final[str] = r"mul\((?P<lhs>\d{1,3}),(?P<rhs>\d{1,3})\)"
+PATTERN_INSTRUCTION: Final[str] = r"(?P<operator>[a-z']+)\((?P<operands>.*)\)"
+# PATTERN_MUL: Final[str] = r"mul\((?P<lhs>\d{1,3}),(?P<rhs>\d{1,3})\)"
 # PATTERN_DO: Final[str] = r"do\(\)"
 # PATTERN_DONT: Final[str] = r"don't\(\)"
 # PATTERN_INSTRUCTION: Final[str] = f"{PATTERN_MUL}|{PATTERN_DO}|{PATTERN_DONT}"
+
+PATTERN_MUL: Final[str] = _build_instruction_pattern(Operator.MUL, OPERAND, OPERAND)
+PATTERN_DO: Final[str] = _build_instruction_pattern(Operator.DO)
+PATTERN_DONT: Final[str] = _build_instruction_pattern(Operator.DONT)
+
+PATTERN_INSTRUCTIONS: Final[str] = "|".join((PATTERN_MUL, PATTERN_DO, PATTERN_DONT))
 
 
 def read_input() -> str:
@@ -50,32 +57,49 @@ def read_input() -> str:
         return file.read()
 
 
+def find_instructions() -> Generator[Instruction, None, None]:
+    instructions: Sequence[str] = re.findall(PATTERN_INSTRUCTIONS, dataset)
+
+    instruction: str
+    for instruction in instructions:
+        instruction_match: Optional[re.Match] = re.match(
+            PATTERN_INSTRUCTION, instruction
+        )
+
+        assert instruction_match is not None
+
+        raw_operator: str
+        raw_operands: str
+        raw_operator, raw_operands = instruction_match.groups()
+
+        operator: Operator = Operator(raw_operator)
+        operands: Sequence[str] = raw_operands.split(",")
+
+        yield Instruction(operator, operands)
+
+
 """Solution for AoC 2024, Day 3, Parts 1 & 2"""
 
 # Load the entire dataset into memory
-input: str = read_input()
+dataset: str = read_input()
 
 # --- Part One ---
 sum_of_multiplications: int = sum(
-    operator.mul(int(lhs), int(rhs))
-    for lhs, rhs in re.findall(r"mul\((?P<lhs>\d{1,3}),(?P<rhs>\d{1,3})\)", input)
+    int(lhs) * int(rhs)
+    for lhs, rhs in re.findall(r"mul\((?P<lhs>\d{1,3}),(?P<rhs>\d{1,3})\)", dataset)
 )
 print("Sum of Multiplications:", sum_of_multiplications)
 assert sum_of_multiplications == 175700056
 
 # --- Part Two ---
 
-# def find_instructions() -> Sequence[str]:
+do: bool = True
 
-# p = _build_instruction_pattern(Operator.MUL, OPERANDS_MUL)
-# p=PATTERN_OPERATOR
-p = "|".join(
-    (
-        _build_instruction_pattern(Operator.MUL, OPERAND, OPERAND),
-        _build_instruction_pattern(Operator.DO),
-    )
-)
-
-instructions: Sequence[str] = re.findall(p, input)
-
-d = instructions
+instruction: Instruction
+for instruction in find_instructions():
+    if instruction.operator is Operator.DO:
+        do = True
+    elif instruction.operator is Operator.DONT:
+        do = False
+    elif instruction.operator is Operator.MUL:
+        print("mul", instruction.operands)
