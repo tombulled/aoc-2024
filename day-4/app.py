91fef10ab765d680cea73b471be441cfef125035
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2024-12-07 18:00:07 +0000

    [day 4, part 1] arrive at a working solution

--- day-4/app.py
+++ day-4/app.py
@@ -3,8 +3,6 @@
 import re
 from typing import Iterable, MutableSequence, Sequence, Tuple
 
-size = 140  # TEMP
-
 
 def read_input() -> str:
     """Read the input file"""
@@ -34,10 +32,10 @@ def to_diagonal(
         for y in range(depth + 1):
             x: int = depth - y
 
-            # true_x: int = abs(anchor[0] - x)
-            # true_y: int = abs(anchor[1] - y)
+            true_x: int = abs(anchor[0] - x)
+            true_y: int = abs(anchor[1] - y)
 
-            character: str = strings[x][y]
+            character: str = strings[true_x][true_y]
 
             string_diagonal += character
 
@@ -52,53 +50,17 @@ def to_diagonal(
         for a in range(size - depth - 1):
             y = depth + a + 1
             x = (dn - a) + depth + 1
-            character: str = strings[x][y]
+
+            true_x: int = abs(anchor[0] - x)
+            true_y: int = abs(anchor[1] - y)
+
+            character: str = strings[true_x][true_y]
+
             string_diagonal += character
 
         strings_diagonal.append(string_diagonal)
 
     return strings_diagonal
-
-
-# def to_diagonal_inv(strings: Sequence[str], /) -> Sequence[str]:
-#     # n.b. assumes a perfect square
-
-#     if not strings:
-#         return ()
-
-#     size: int = len(strings[0])
-
-#     strings_diagonal: MutableSequence[str] = []
-
-#     depth: int
-#     for depth in range(size):
-#         string_diagonal: str = ""
-
-#         y: int
-#         for y in range(depth+1):
-#             x: int = depth - y
-
-#             character: str = strings[size-x-1][size-y-1]
-
-#             string_diagonal += character
-
-#         strings_diagonal.append(string_diagonal)
-
-#     depth: int
-#     for depth in range(size-1):
-#         dn = size-depth-2
-
-#         string_diagonal: str = ""
-
-#         for a in range(size-depth-1):
-#             y=depth+a+1
-#             x=(dn-a)+depth+1
-#             character: str = strings[size-x-1][size-y-1]
-#             string_diagonal += character
-
-#         strings_diagonal.append(string_diagonal)
-
-#     return strings_diagonal
 
 
 def count_xmas_in_string(string: str, /) -> int:
@@ -146,14 +108,16 @@ dataset: str = read_input()
 
 lines_horizontal: Sequence[str] = dataset.splitlines()
 lines_vertical: Sequence[str] = tuple("".join(line) for line in zip(*lines_horizontal))
-lines_diagonal: Sequence[str] = to_diagonal(lines_horizontal)
-lines_diagonal_inv: Sequence[str] = to_diagonal_inv(lines_horizontal)
+
+max_index: int = len(lines_horizontal[0])-1
 
 seq_of_lines: Sequence[Sequence[str]] = (
     lines_horizontal,
     lines_vertical,
-    lines_diagonal,
-    lines_diagonal_inv,
+    to_diagonal(lines_horizontal, anchor=(0, 0)),
+    to_diagonal(lines_horizontal, anchor=(0, max_index)),
+    # to_diagonal(lines_horizontal, anchor=(max_index, 0)),
+    # to_diagonal(lines_horizontal, anchor=(max_index, max_index)),
 )
 
 total_xmas_occurences: int = 0
@@ -166,3 +130,8 @@ for lines in seq_of_lines:
     total_xmas_occurences += count_xmas_in_strings(lines_inv)
 
 print("Part 1:", total_xmas_occurences)
+
+# d1 = to_diagonal(lines_horizontal, anchor=(0, 0)) # top left to bottom right
+# d2 = to_diagonal(lines_horizontal, anchor=(0, max_index)) # top right to bottom left
+# d3 = to_diagonal(lines_horizontal, anchor=(max_index, 0)) # bottom left to top right
+# d4 = to_diagonal(lines_horizontal, anchor=(max_index, max_index)) # bottom right to top left
