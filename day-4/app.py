77ba93c209794cbf883294f137af8a025a1f63c2
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2024-12-07 18:05:28 +0000

    [day 4, part 1] tidy up part one a bit

--- day-4/app.py
+++ day-4/app.py
@@ -1,7 +1,9 @@
 """Day 4: Ceres Search"""
 
 import re
-from typing import Iterable, MutableSequence, Sequence, Tuple
+from typing import Final, Iterable, MutableSequence, Sequence, Tuple
+
+SIZE: Final[int] = 140
 
 
 def read_input() -> str:
@@ -15,7 +17,7 @@ def read_input() -> str:
 def to_diagonal(
     strings: Sequence[str], /, *, anchor: Tuple[int, int] = (0, 0)
 ) -> Sequence[str]:
-    # n.b. assumes a perfect square
+    """Produce a sequence of diagonal strings from a given sequence (assuming a perfect square)"""
 
     if not strings:
         return ()
@@ -25,38 +27,40 @@ def to_diagonal(
     strings_diagonal: MutableSequence[str] = []
 
     depth: int
+    string_diagonal: str
+    x: int
+    y: int
+    true_x: int
+    true_y: int
+
+    depth
     for depth in range(size):
-        string_diagonal: str = ""
+        string_diagonal = ""
 
-        y: int
         for y in range(depth + 1):
-            x: int = depth - y
+            x = depth - y
 
-            true_x: int = abs(anchor[0] - x)
-            true_y: int = abs(anchor[1] - y)
+            true_x = abs(anchor[0] - x)
+            true_y = abs(anchor[1] - y)
 
-            character: str = strings[true_x][true_y]
-
-            string_diagonal += character
+            string_diagonal += strings[true_x][true_y]
 
         strings_diagonal.append(string_diagonal)
 
-    depth: int
+    depth
     for depth in range(size - 1):
-        dn = size - depth - 2
+        true_depth = size - depth - 2
 
-        string_diagonal: str = ""
+        string_diagonal = ""
 
         for a in range(size - depth - 1):
             y = depth + a + 1
-            x = (dn - a) + depth + 1
+            x = (true_depth - a) + depth + 1
 
-            true_x: int = abs(anchor[0] - x)
-            true_y: int = abs(anchor[1] - y)
+            true_x = abs(anchor[0] - x)
+            true_y = abs(anchor[1] - y)
 
-            character: str = strings[true_x][true_y]
-
-            string_diagonal += character
+            string_diagonal += strings[true_x][true_y]
 
         strings_diagonal.append(string_diagonal)
 
@@ -73,51 +77,21 @@ def count_xmas_in_strings(strings: Sequence[str], /) -> int:
 
 # Load the entire dataset into memory
 dataset: str = read_input()
-# dataset: str = (
-#     """
-# MMMSXXMASM
-# MSAMXMSMSA
-# AMXSXMAAMM
-# MSAMASMSMX
-# XMASAMXAMM
-# XXAMMXXAMA
-# SMSMSASXSS
-# SAXAMASAAA
-# MAMMMXMMMM
-# MXMXAXMASX""".strip()
-# )
 
-# dataset: str = """
-# ABC
-# BCD
-# CDE
-# """.strip()
-# dataset: str = """
-# ABCD
-# BCDE
-# CDEF
-# DEFG
-# """.strip()
-# dataset: str = """
-# ABCDE
-# BCDEF
-# CDEFG
-# DEFGH
-# EFGHI
-# """.strip()
+# --- Part One ---
 
 lines_horizontal: Sequence[str] = dataset.splitlines()
 lines_vertical: Sequence[str] = tuple("".join(line) for line in zip(*lines_horizontal))
-
-max_index: int = len(lines_horizontal[0])-1
+lines_diagonal_left: Sequence[str] = to_diagonal(lines_horizontal, anchor=(0, 0))
+lines_diagonal_right: Sequence[str] = to_diagonal(
+    lines_horizontal, anchor=(0, SIZE - 1)
+)
 
 seq_of_lines: Sequence[Sequence[str]] = (
     lines_horizontal,
     lines_vertical,
-    to_diagonal(lines_horizontal, anchor=(0, 0)),
-    to_diagonal(lines_horizontal, anchor=(0, max_index)),
-    # to_diagonal(lines_horizontal, anchor=(max_index, 0)),
-    # to_diagonal(lines_horizontal, anchor=(max_index, max_index)),
+    lines_diagonal_left,
+    lines_diagonal_right,
 )
 
 total_xmas_occurences: int = 0
@@ -130,8 +104,8 @@ for lines in seq_of_lines:
     total_xmas_occurences += count_xmas_in_strings(lines_inv)
 
 print("Part 1:", total_xmas_occurences)
+assert total_xmas_occurences == 2468
 
-# d1 = to_diagonal(lines_horizontal, anchor=(0, 0)) # top left to bottom right
-# d2 = to_diagonal(lines_horizontal, anchor=(0, max_index)) # top right to bottom left
-# d3 = to_diagonal(lines_horizontal, anchor=(max_index, 0)) # bottom left to top right
-# d4 = to_diagonal(lines_horizontal, anchor=(max_index, max_index)) # bottom right to top left
+# --- Part Two ---
+
+# ...
