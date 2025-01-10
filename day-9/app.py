5cfb6cd8b8320c3b0cf4435cd6e732bdbef7d50e
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2025-01-10 11:15:10 +0000

    [day 9, part 1] First stab at a disk compacting algorithm

--- day-9/app.py
+++ day-9/app.py
@@ -1,12 +1,18 @@
 """Day 9: Disk Fragmenter"""
 
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
+import dataclasses
 from enum import IntEnum, auto
-from typing import Final, Iterable, Self, Type
+from typing import Final, Iterable, MutableSequence, Self, Sequence, Type, TypeAlias
+
+# Typing
+Disk: TypeAlias = Sequence["Block"]
+MutableDisk: TypeAlias = MutableSequence["Block"]
 
 EXAMPLE_INPUT: Final[str] = "2333133121414131402"
 
-FREE: Final[str] = "."  # WARN: NOT USED??
+# FREE: Final[str] = "."  # WARN: NOT USED??
 
 
 class _CyclicalEnum(IntEnum):
@@ -31,17 +37,26 @@ class DiskMapEntryType(_CyclicalEnum):
 
 
 @dataclass
-class Block:
+class Block(ABC):
     size: int
+
+    @abstractmethod
+    def render(self) -> str:
+        raise NotImplementedError
 
 
 class FreeSpace(Block):
-    pass
+    # pass
+    def render(self) -> str:
+        return "." * self.size
 
 
 @dataclass
 class File(Block):
     id: int
+
+    def render(self) -> str:
+        return str(self.id) * self.size
 
 
 def read_dataset() -> str:
@@ -66,10 +81,52 @@ def parse_disk_map(disk_map: str, /) -> Iterable[Block]:
 
         entry_type = entry_type.next()
 
+def get_disk_size(disk: Disk, /) -> int:
+    return sum(block.size for block in disk)
+
+def compact_disk(disk: MutableDisk, /) -> MutableDisk:
+    new_disk: MutableDisk = []
+
+    block: Block
+    for block in disk:
+        if not isinstance(block, FreeSpace):
+            if block.size > 0:
+                new_disk.append(block)
+
+            continue
+
+        free_space: FreeSpace = block
+
+        candidate_block: Block
+        for candidate_block in reversed(disk):
+            if isinstance(candidate_block, FreeSpace) or candidate_block.size == 0:
+                continue
+
+            chunk_size: int = min(free_space.size, candidate_block.size)
+            block_chunk: Block = dataclasses.replace(candidate_block, size=chunk_size)
+
+            new_disk.append(block_chunk)
+            free_space.size -= chunk_size
+
+        if free_space.size > 0:
+            new_disk.append(free_space)
+
+    return new_disk
+
+def render_disk(disk: Disk, /) -> str:
+    return "".join(block.render() for block in disk)
+
+def print_disk(disk: Disk, /) -> None:
+    print(render_disk(disk))
 
 # dataset: str = read_dataset()
 dataset: str = EXAMPLE_INPUT
-blocks: Iterable[Block] = parse_disk_map(dataset)
+disk: MutableDisk = list(parse_disk_map(dataset))
 
-for block in blocks:
-    print(block)
+# for block in blocks:
+#     print(block)
+
+compacted_disk: MutableDisk = compact_disk(disk)
+
+print_disk(disk)
+print_disk(compacted_disk)
